14bdba8702e0dd7b57b74fa2e9e068ff610f9be5
Tommy Marcheschi <tommymarcheschi@Tommys-MacBook-Pro.local>
2023-10-30 17:31:52 -0500

    env

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -32,7 +32,7 @@
     <div class="grid gap-6 sm:grid-cols-2">
       <div class="relative z-0">
         <input bind:value={name} type="text" name="name" class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-white focus:border-white focus:outline-none focus:ring-0" placeholder=" " />
-        <label class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-white peer-focus:dark:text-white">Your name</label>
+        <label class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-white peer-focus:dark:text-white font-anon">Your name</label>
       </div>
       <div class="relative z-0">
         <input bind:value={email} type="text" name="email" class="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-white focus:border-white focus:outline-none focus:ring-0" placeholder=" " />
